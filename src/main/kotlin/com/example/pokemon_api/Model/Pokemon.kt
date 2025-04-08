1c0ab96bc30412a66ab1aae7f551a10cd8597fbb
DanielM3218 <daniel.inon.melki@gmail.com>
2025-04-08 10:32:26 +0200

    working projecty model service controller in dev branch

---
+++ src/main/kotlin/com/example/pokemon_api/Model/Pokemon.kt
@@ -0,0 +1,9 @@
+package com.example.pokemon_api.Model
+
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val types: List<String>,
+    val pokedexNumber: Int,
+    val imageUrl: String
+)
